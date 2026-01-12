2327d409c0d7d9c9a719656c32b7071ea65b880a
Patrik Svensson <patrik.svensson@manyone.com>
2026-01-12 16:20:01 +0100

    fix: Portal closing nicely on mobile (was disappearing while transitioning)

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Metervara 2026
 
 The 2026 version of Metevara.xyz
-
--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,8 @@ import './style.css'
 
 let destroyEye: (() => void) | null = null;
 
-// document.documentElement.style.setProperty('--mv-transition-duration', '0s');
-
 window.addEventListener("DOMContentLoaded", () => {
   console.log("Welcome to Metervara.");
-
-  // requestAnimationFrame(() => {
-  //   document.documentElement.style.removeProperty('--mv-transition-duration');
-  // });
 
   const { portal } = setupDomPortal({
     portalTarget: "#app",
@@ -26,30 +20,18 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// Portal events 
-
+// Portal events
 
 window.addEventListener('portal:opening', (_e: Event) => {
-  // console.log('portal:opening')
   requestAnimationFrame(() => {
     const eyeRoot = document.querySelector('.metervara-portal-content-container .portal-content-eye .eye-container') as HTMLElement;
     destroyEye = createEye(eyeRoot).destroy;
   });
 });
 
-window.addEventListener('portal:closing', (_e: Event) => {
-  // console.log('portal:closing')
+window.addEventListener('portal:closed', (_e: Event) => {
   if (destroyEye) {
     destroyEye();
     destroyEye = null;
   }
 });
-
-/*
-window.addEventListener('portal:open', (_e: Event) => {
-  console.log('portal:open')
-});
-window.addEventListener('portal:closed', (_e: Event) => {
-  console.log('portal:closed')
-});
-*/
